ebdae707766d83894fc485c3a7f6a77adef9e613
yangchaoben <842293890@qq.com>
2016-01-12 22:46:26 +0800

    增加service

--- src/js/servies/search.js
+++ src/js/servies/search.js
@@ -1,7 +1,42 @@
 var search=angular.module('search',[])
 
-search.controller('searchClassCtr',['$scope',function($scope){
+search.controller('searchClassCtr',['$scope','searchClassSer',function($scope,searchClassSer){
 	//获取商品分类
+	$scope.datas=searchClassSer.result.pageResultList.result;
+	//获取对应分类下的商品
+	$scope.finditem=function(id){
+		$.ajax({
+			url: 'http://211.149.150.213:9090/application-shopapp/productInfo/queryProductInfo.tz',
+		    type: 'post',
+		    dataType: 'json',
+		   	async:false,
+		    headers: {
+		         "imei":"asdaSA",
+		    "mobileOperators":"IOS8",
+		    "originateEnum":"APP_USER_AND",
+		    "version":"2_0",
+		    "sysVersion":"ios9",
+		    "phoneModel":"iphone"
+
+		    },
+		    data:{
+		    	productId:id
+
+		    },
+		    success: function(data){
+		    	$scope.datas=data.result.pageResultList.result;
+		    	console.log($scope.datas)
+		    },
+		    error: function(data){
+		        alert(data);
+		    }
+		})
+		
+	}
+}])
+
+search.service("searchClassSer",function(){
+	var datas="";
 	$.ajax({
 		url: 'http://211.149.150.213:9090/application-shopapp/productItem/selectProductItem.tz',
         type: 'post',
@@ -17,15 +52,11 @@ search.controller('searchClassCtr',['$scope',function($scope){
 
         },
         success: function(data){
-        	$scope.datas=data.result.pageResultList.result;
-        	console.log($scope.datas)
+        	datas=data
         },
         error: function(data){
             alert(data);
         }
 	})
-	//获取对应分类下的商品
-	$scope.finditem=function(id){
-		alert(id)
-	}
-}])
+	return datas
+})
