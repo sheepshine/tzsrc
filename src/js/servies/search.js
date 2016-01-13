fe953b81c5bbb53676d253256d8f36185f25e7dd
yangchaoben <842293890@qq.com>
2016-01-13 13:52:46 +0800

    模块修改

--- src/js/servies/search.js
+++ src/js/servies/search.js
@@ -1,37 +1,41 @@
 var search=angular.module('search',[])
 
-search.controller('searchClassCtr',['$scope','searchClassSer',function($scope,searchClassSer){
+search.controller('searchClassCtr',['$scope','searchClassSer','searchItemSera',function($scope,searchClassSer,searchItemSera){
 	//获取商品分类
 	$scope.datas=searchClassSer.result.pageResultList.result;
 	//获取对应分类下的商品
-	$scope.finditem=function(id){
-		$.ajax({
-			url: 'http://211.149.150.213:9090/application-shopapp/productInfo/queryProductInfo.tz',
-		    type: 'post',
-		    dataType: 'json',
-		   	async:false,
-		    headers: {
-		         "imei":"asdaSA",
-		    "mobileOperators":"IOS8",
-		    "originateEnum":"APP_USER_AND",
-		    "version":"2_0",
-		    "sysVersion":"ios9",
-		    "phoneModel":"iphone"
+	//$scope.finditem=searchItemSer(id);
+	//console.log($scope.finditem)
+	$scope.finditem=searchItemSera.searchItem
+	console.log(searchItemSera.datasa)
+	// function(id){
+	// 	$.ajax({
+	// 		url: 'http://211.149.150.213:9090/application-shopapp/productInfo/queryProductInfo.tz',
+	// 	    type: 'post',
+	// 	    dataType: 'json',
+	// 	   	async:false,
+	// 	    headers: {
+	// 	         "imei":"asdaSA",
+	// 	    "mobileOperators":"IOS8",
+	// 	    "originateEnum":"APP_USER_AND",
+	// 	    "version":"2_0",
+	// 	    "sysVersion":"ios9",
+	// 	    "phoneModel":"iphone"
 
-		    },
-		    data:{
-		    	shopItemClassId:id
+	// 	    },
+	// 	    data:{
+	// 	    	shopItemClassId:id
 
-		    },
-		    success: function(data){
+	// 	    },
+	// 	    success: function(data){
 		    	
-		    },
-		    error: function(data){
-		        alert(data);
-		    }
-		})
+	// 	    },
+	// 	    error: function(data){
+	// 	        alert(data);
+	// 	    }
+	// 	})
 		
-	}
+	// }
 }])
 
 search.service("searchClassSer",function(){
@@ -42,14 +46,13 @@ search.service("searchClassSer",function(){
         dataType: 'json',
        	async:false,
         headers: {
-             "imei":"asdaSA",
-        "mobileOperators":"IOS8",
-        "originateEnum":"APP_USER_AND",
-        "version":"2_0",
-        "sysVersion":"ios9",
-        "phoneModel":"iphone"
-
-        },
+            "imei":"asdaSA",
+	        "mobileOperators":"IOS8",
+	        "originateEnum":"APP_USER_AND",
+	        "version":"2_0",
+	        "sysVersion":"ios9",
+	        "phoneModel":"iphone"
+		},
         success: function(data){
         	datas=data
         },
@@ -58,4 +61,36 @@ search.service("searchClassSer",function(){
         }
 	})
 	return datas
+})
+
+search.service("searchItemSera",function(){
+	return {
+		searchItem:function(id){
+			var datasa="";
+			$.ajax({
+				url: 'http://211.149.150.213:9090/application-shopapp/productInfo/queryProductInfo.tz',
+		        type: 'post',
+		        dataType: 'json',
+		       	async:false,
+		       	data:{
+				   	shopItemClassId:id
+				},
+		        headers: {
+		            "imei":"asdaSA",
+			        "mobileOperators":"IOS8",
+			        "originateEnum":"APP_USER_AND",
+			        "version":"2_0",
+			        "sysVersion":"ios9",
+			        "phoneModel":"iphone"
+				},
+		        success: function(data){
+		        	datasa=data
+		        },
+		        error: function(data){
+		            alert(data);
+		        }
+			})
+		}
+
+	}
 })
